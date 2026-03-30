d616adc55b0186b12ec701b83c3ff1f9db6ddb16
F3ll4s <sirapop3932@gmail.com>
2026-03-30 23:07:13 +0700

    register_page

--- frontend/frontend/frontend.py
+++ frontend/frontend/frontend.py
@@ -1,7 +1,9 @@
 import reflex as rx
 
 from .state import State
-from frontend.pages.register import register
+from frontend.pages.register import register_page
+from frontend.pages.login import login
 
 app = rx.App()
-app.add_page(register)
+app.add_page(register_page,route="/register")
+app.add_page(login)
---
+++ frontend/frontend/pages/login.py
@@ -0,0 +1,76 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+
+from rxconfig import config
+
+
+class RadioGroupState(rx.State):
+    item: str = "No Selection"
+
+    @rx.event
+    def set_item(self, item: str):
+        self.item = item
+        
+def login() -> rx.Component:
+    return rx.center(
+    rx.box(
+        rx.flex(
+            rx.box(
+                rx.center(
+                    rx.image(
+                        #Put image
+                    ),
+                    height="100%"
+                ),
+                flex="1",
+                border="1px solid black"# equal width
+            ),  
+            rx.box(
+                rx.center(
+                    rx.vstack(
+                        rx.heading(
+                            "Login",
+                            width="100%",
+                            text_align="center",
+                            margin_bottom="30px"
+                        ),
+
+                        rx.text("Username",margin="10px 0 5px 20px",font_size="15px"),
+                        rx.input(placeholder="Username", width="400px",bg="white",margin_bottom="10px",height="45px",border_radius="40px",padding_left="20px",color="black",font_size="17px"),
+
+                        rx.text("Email",margin="10px 0 5px 20px",font_size="15px"),
+                        rx.input(placeholder="Email", width="400px",bg="white",margin_bottom="10px",height="45px",border_radius="40px",padding_left="20px",color="black",font_size="17px"),
+                        
+                        rx.text("Password",margin="10px 0 5px 20px",font_size="15px"),
+                        rx.input(placeholder="Password", width="400px",bg="white",margin_bottom="10px",height="45px",border_radius="40px",padding_left="20px",color="black",font_size="17px"),
+                        
+                        rx.button(
+                            "Submit",
+                            width="100%",
+                            text_color="white"
+                        ),
+                        
+                        
+                        align="start",  
+                        spacing="0"
+                    ),
+                    height="90%"
+                ),
+                flex="1",
+                border="1px solid black"# equal width
+            ),
+
+            spacing="3",   # space between boxes
+            width="100%",
+            height="100%"
+        ),
+        width="1000px",
+        height="600px",
+        padding="20px",
+        border_radius="60px",
+        bg="linear-gradient(to right, #4facfe, #00f2fe)"
+    ),
+    height="100vh",
+    bg="gray"
+)
