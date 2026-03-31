0318f7e7bef11c5e73fb1aafbf0c41cbcb44adcb
F3ll4s <sirapop3932@gmail.com>
2026-03-31 19:32:14 +0700

    all room

--- frontend/frontend/frontend.py
+++ frontend/frontend/frontend.py
@@ -4,8 +4,11 @@ from .state import State
 from frontend.pages.register import register_page
 from frontend.pages.login import login_page
 from frontend.pages.index import index
+from frontend.pages.AllRoom import hotel_page
+
 
 app = rx.App()
 app.add_page(index, route="/")
 app.add_page(register_page,route="/register")
 app.add_page(login_page,route="/login")
+app.add_page(hotel_page,route="/resource/rooms")
