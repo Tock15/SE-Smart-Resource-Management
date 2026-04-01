8d163f4ef8fb0da5c9eaaac7354d377f60321152
Koses Suvarnasuddhi <koses.su@gmail.com>
2026-04-01 15:09:06 +0700

    refactor booking info and started booking guests

--- backend/app/models/booking.py
+++ backend/app/models/booking.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum, func
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum, Table, func
 from sqlalchemy.orm import relationship
 from app.db.base import Base
 import enum
@@ -19,6 +19,12 @@ class Timeslot(Base):
     # Relationship to Booking
     booking = relationship("Booking", back_populates="timeslot", uselist=False)
 
+booking_guests = Table(
+    "booking_guests",
+    Base.metadata,
+    Column("booking_id", Integer, ForeignKey("bookings.booking_id"), primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.user_id"), primary_key=True)
+)
 class Booking(Base):
     __tablename__ = "bookings"
 
@@ -28,8 +34,10 @@ class Booking(Base):
     timeslot_id = Column(Integer, ForeignKey("timeslots.timeslot_id"), nullable=False)
     status = Column(Enum(BookingStatus), default=BookingStatus.PENDING, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
+    guests = relationship("User", secondary=booking_guests, backref="guest_bookings")
 
     # Relationships
     user = relationship("User")
     resource = relationship("Resource")
     timeslot = relationship("Timeslot", back_populates="booking")
+
